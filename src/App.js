e70663e685afc4005d6aec14b41ef08832ec764c
Gabriel Monteiro Fernandes <gamofe@protonmail.ch>
2019-07-16 09:59:58 -0300

    Módulo 09 - Estilos globais

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Router } from 'react-router-dom'
 import history from './services/history'
 import Routes from './routes'
+import Style from './styles/App'
 import './config/reactotron'
 
 function App() {
   return (
     <Router history={history}>
       <Routes />
+      <Style />
     </Router>
   )
 }
